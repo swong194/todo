127928dd9f8ae666da408a3c92703ea31f7cea46
Sunny Wong <swong194@gmail.com>
2021-04-24 21:44:14 -0400

    update test configs and gems

--- Gemfile
+++ Gemfile
@@ -33,6 +33,9 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 5.0.0'
   gem 'faker'
+  gem 'factory_bot', require: false
+  gem 'database_cleaner'
+  gem 'pry'
 end
 
 group :development do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,27 @@ require_relative '../config/environment'
 require 'rails/test_help'
 require 'faker'
 require 'rspec/rails'
+require 'factory_bot'
+require 'database_cleaner'
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+  FactoryBot.definition_file_paths = [File.expand_path('../factories', __FILE__)]
+  FactoryBot.reload
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
